9bebe477b8b761dbaba4a2f17aa84ca680581c9b
alexis <diaz@localhost.localdomain>
2019-05-23 23:43:49 -0500

    Push desde mi rama

--- Login/views.py
+++ Login/views.py
@@ -17,3 +17,19 @@ class CustomAuthToken(ObtainAuthToken):
             'user_id': user.pk,
             'username': user.username
         })
+
+
+class CustomAuthToken1(ObtainAuthToken):
+    
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data = request.data,
+        context = {'request':request}) 
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'username': user.username
+        })
